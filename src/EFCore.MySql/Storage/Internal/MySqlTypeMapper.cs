496e671f37a2f9dd5cd814b474e51f3d7b71728a
Caleb Lloyd <caleblloyd@gmail.com>
2018-03-15 08:59:00 -0400

    type mapper logging

--- src/EFCore.MySql/Storage/Internal/MySqlTypeMapper.cs
+++ src/EFCore.MySql/Storage/Internal/MySqlTypeMapper.cs
@@ -15,50 +15,49 @@ namespace EFCore.MySql.Storage.Internal
     public class MySqlTypeMapper : RelationalTypeMapper
     {
         // boolean
-        private readonly MySqlBoolTypeMapping _bit          = new MySqlBoolTypeMapping("bit", DbType.Boolean);
+        private readonly MySqlBoolTypeMapping _bit            = new MySqlBoolTypeMapping("bit", DbType.Boolean);
 
         // integers
-        private readonly SByteTypeMapping _tinyint          = new SByteTypeMapping("tinyint", DbType.SByte);
-        private readonly ByteTypeMapping _utinyint          = new ByteTypeMapping("tinyint unsigned", DbType.Byte);
-	    private readonly ShortTypeMapping _smallint         = new ShortTypeMapping("smallint", DbType.Int16);
-	    private readonly UShortTypeMapping _usmallint       = new UShortTypeMapping("smallint unsigned", DbType.UInt16);
-        private readonly IntTypeMapping _int                = new IntTypeMapping("int", DbType.Int32);
-	    private readonly UIntTypeMapping _uint              = new UIntTypeMapping("int unsigned", DbType.UInt32);
-	    private readonly LongTypeMapping _bigint            = new LongTypeMapping("bigint", DbType.Int64);
-	    private readonly ULongTypeMapping _ubigint          = new ULongTypeMapping("bigint unsigned", DbType.UInt64);
+        private readonly SByteTypeMapping  _tinyint           = new SByteTypeMapping("tinyint", DbType.SByte);
+        private readonly ByteTypeMapping   _utinyint          = new ByteTypeMapping("tinyint unsigned", DbType.Byte);
+	    private readonly ShortTypeMapping  _smallint          = new ShortTypeMapping("smallint", DbType.Int16);
+	    private readonly UShortTypeMapping _usmallint         = new UShortTypeMapping("smallint unsigned", DbType.UInt16);
+        private readonly IntTypeMapping    _int               = new IntTypeMapping("int", DbType.Int32);
+	    private readonly UIntTypeMapping   _uint              = new UIntTypeMapping("int unsigned", DbType.UInt32);
+	    private readonly LongTypeMapping   _bigint            = new LongTypeMapping("bigint", DbType.Int64);
+	    private readonly ULongTypeMapping  _ubigint           = new ULongTypeMapping("bigint unsigned", DbType.UInt64);
 
 	    // decimals
-	    private readonly DecimalTypeMapping _decimal        = new DecimalTypeMapping("decimal(65, 30)", DbType.Decimal);
-	    private readonly DoubleTypeMapping _double          = new DoubleTypeMapping("double", DbType.Double);
-        private readonly FloatTypeMapping _float            = new FloatTypeMapping("float");
+	    private readonly DecimalTypeMapping _decimal          = new DecimalTypeMapping("decimal(65, 30)", DbType.Decimal);
+	    private readonly DoubleTypeMapping  _double           = new DoubleTypeMapping("double", DbType.Double);
+        private readonly FloatTypeMapping   _float            = new FloatTypeMapping("float");
 
 	    // binary
-	    private readonly RelationalTypeMapping _binary           = new MySqlByteArrayTypeMapping("binary", DbType.Binary);
-        private readonly RelationalTypeMapping _varbinary        = new MySqlByteArrayTypeMapping("varbinary", DbType.Binary);
-	    private readonly MySqlByteArrayTypeMapping _varbinary767 = new MySqlByteArrayTypeMapping("varbinary(767)", DbType.Binary, 767);
-	    private readonly RelationalTypeMapping _varbinarymax     = new MySqlByteArrayTypeMapping("longblob", DbType.Binary);
+	    private readonly RelationalTypeMapping     _binary           = new MySqlByteArrayTypeMapping("binary", DbType.Binary);
+        private readonly RelationalTypeMapping     _varbinary        = new MySqlByteArrayTypeMapping("varbinary", DbType.Binary);
+	    private readonly MySqlByteArrayTypeMapping _varbinary767     = new MySqlByteArrayTypeMapping("varbinary(767)", DbType.Binary, 767);
+	    private readonly RelationalTypeMapping     _varbinarymax     = new MySqlByteArrayTypeMapping("longblob", DbType.Binary);
 
 	    // string
-        private readonly MySqlStringTypeMapping _char            = new MySqlStringTypeMapping("char", DbType.AnsiStringFixedLength);
-        private readonly MySqlStringTypeMapping _varchar         = new MySqlStringTypeMapping("varchar", DbType.AnsiString);
-	    private readonly MySqlStringTypeMapping _varchar127      = new MySqlStringTypeMapping("varchar(127)", DbType.AnsiString, true, 127);
-	    private readonly MySqlStringTypeMapping _varcharmax      = new MySqlStringTypeMapping("longtext", DbType.AnsiString);
+        private readonly MySqlStringTypeMapping _char         = new MySqlStringTypeMapping("char", DbType.AnsiStringFixedLength);
+        private readonly MySqlStringTypeMapping _varchar      = new MySqlStringTypeMapping("varchar", DbType.AnsiString);
+	    private readonly MySqlStringTypeMapping _varchar127   = new MySqlStringTypeMapping("varchar(127)", DbType.AnsiString, true, 127);
+	    private readonly MySqlStringTypeMapping _varcharmax   = new MySqlStringTypeMapping("longtext", DbType.AnsiString);
 
         // DateTime
-        private readonly MySqlDateTimeTypeMapping _dateTime6              = new MySqlDateTimeTypeMapping("datetime(6)", DbType.DateTime);
-        private readonly MySqlDateTypeMapping _date                       = new MySqlDateTypeMapping("date", DbType.Date);
-        private readonly MySqlDateTimeOffsetTypeMapping _dateTimeOffset6  = new MySqlDateTimeOffsetTypeMapping("datetime(6)", DbType.DateTime);
-        private readonly MySqlDateTimeOffsetTypeMapping _timeStamp6       = new MySqlDateTimeOffsetTypeMapping("timestamp(6)", DbType.DateTime);
-        private readonly TimeSpanTypeMapping _time6                       = new TimeSpanTypeMapping("time(6)", DbType.Time);
+        private readonly MySqlDateTimeTypeMapping       _dateTime6   = new MySqlDateTimeTypeMapping("datetime(6)", DbType.DateTime);
+        private readonly MySqlDateTypeMapping           _date        = new MySqlDateTypeMapping("date", DbType.Date);
+        private readonly MySqlDateTimeOffsetTypeMapping _timeStamp6  = new MySqlDateTimeOffsetTypeMapping("timestamp(6)", DbType.DateTime);
+        private readonly TimeSpanTypeMapping            _time6       = new TimeSpanTypeMapping("time(6)", DbType.Time);
 
         // json
-        private readonly RelationalTypeMapping _json         = new MySqlJsonTypeMapping("json", DbType.String);
+        private readonly RelationalTypeMapping _json          = new MySqlJsonTypeMapping("json", DbType.String);
 
 	    // row version
-        private readonly RelationalTypeMapping _rowversion   = new MySqlByteArrayTypeMapping("TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP", DbType.Binary);
+        private readonly RelationalTypeMapping _rowversion    = new MySqlByteArrayTypeMapping("TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP", DbType.Binary);
 
         // guid
-	    private readonly GuidTypeMapping _uniqueidentifier   = new GuidTypeMapping("char(36)", DbType.Guid);
+	    private readonly GuidTypeMapping _uniqueidentifier    = new GuidTypeMapping("char(36)", DbType.Guid);
 
         readonly Dictionary<string, RelationalTypeMapping> _storeTypeMappings;
         readonly Dictionary<Type, RelationalTypeMapping> _clrTypeMappings;
@@ -145,7 +144,7 @@ namespace EFCore.MySql.Storage.Internal
 
 	                // DateTime
 	                { typeof(DateTime), _dateTime6 },
-	                { typeof(DateTimeOffset), _dateTimeOffset6 },
+	                { typeof(DateTimeOffset), _timeStamp6 },
 	                { typeof(TimeSpan), _time6 },
 
 	                // json
@@ -254,11 +253,28 @@ namespace EFCore.MySql.Storage.Internal
             if (clrType.Name == typeof(JsonObject<>).Name)
                 return _json;
 
-            return clrType == typeof(string)
+            var mapping = clrType == typeof(string)
                 ? _varcharmax
                 : (clrType == typeof(byte[])
                     ? _varbinarymax
                     : base.FindMapping(clrType));
+
+            Console.WriteLine($"MySqlTypeMapper: FindMapping(Type clrType); clrType = {clrType}; mapping = {mapping}");
+            return mapping;
+        }
+
+        public override RelationalTypeMapping FindMapping(IProperty property)
+        {
+            var mapping = base.FindMapping(property);
+            Console.WriteLine($"MySqlTypeMapper: FindMapping(IProperty property); property.Name = {property.Name}; mapping = {mapping}");
+            return mapping;
+        }
+
+        public override RelationalTypeMapping FindMapping(string storeType)
+        {
+            var mapping = base.FindMapping(storeType);
+            Console.WriteLine($"MySqlTypeMapper: FindMapping(string storeType); storeType = {storeType}; mapping = {mapping}");
+            return mapping;
         }
 
         // Indexes in SQL Server have a max size of 900 bytes
